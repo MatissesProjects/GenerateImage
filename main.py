5cacf088a679d2601c1ce3b21ad598e9d80bcbb8
Matisse <MatissesProjects@users.noreply.github.com>
2024-08-19 12:20:25 -0700

    adding how to and callout to matissetec.dev

--- main.py
+++ main.py
@@ -38,6 +38,15 @@ def render_readme(imageLocation):
                 f"[<img src='{imageLocation}'>](https://github.com/{GITHUB_REPO}/issues/new?title=Transform:%20&body=No%20need%20to%20modify%20the%20body,%20just%20add%20your%20transformation%20to%20the%20photo%20in%20the%20title)",
                 "",
                 "The images takes around 30s to generate, please be patient.",
+                "",
+                "## How to use",
+                "1. Click the image above",
+                "2. Add your transformation to the title",
+                "3. Submit the issue",
+                "4. Wait for the new image to be generated, around 30 seconds",
+                "",
+                "## Want faster results?",
+                "Try the page this api is based on: [DeepNarration](https://deepnarration.matissetec.dev/)",
             ]
     return "\n".join(lines)
 
