ae1d6f1cb962a507bef0754cb3aa585cbddc3e3f
Matisse <MatissesProjects@users.noreply.github.com>
2024-09-07 11:59:32 -0700

    clearing up what next round is

--- PlayGame/manageGameState.py
+++ PlayGame/manageGameState.py
@@ -82,7 +82,7 @@ def playGame(issue):
         close_with_error(issue, "Error generating image, the response was wrong")
         return
     print("resulting image location: ", imageLocation)
-    issue.create_comment(f"Here is your image: {imageLocation}\nDo you want to enter this for your current difficulty level? (one entry per difficulty level)\n\n- ![Yes](https://github.com/MatissesProjects/GenerateImage/issues/new?template=enterCurrentImage.yml)")
+    issue.create_comment(f"Here is your image: {imageLocation}\nDo you want to enter this for your current difficulty level? (one entry per difficulty level)\n\n- ![Yes](https://github.com/MatissesProjects/GenerateImage/issues/new?template=enterCurrentImage.yml) if no you are done, or optionally you can answer the form no.")
 
 def main():
     print("ISSUE_NUMBER:", ISSUE_NUMBER)
