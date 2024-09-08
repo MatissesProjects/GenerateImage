819c9cf686a0e255e27dc46db8df3ddbacf5f134
Matisse <MatissesProjects@users.noreply.github.com>
2024-09-08 14:53:37 -0700

    change targetURL to TARGET_URL lists -> list

--- workflowScripts/reset-game-values.sh
+++ workflowScripts/reset-game-values.sh
@@ -4,4 +4,4 @@ echo '{"easy":{},"medium":{},"hard":{}}' > PlayGame/currentVotes.json
 echo '{"easy":{},"medium":{},"hard":{}}' > PlayGame/currentEntries.json
 echo -e "# Votes\nnew competition started empty votes\n" > PlayGame/VotePage/CurrentVotes.md
 echo -e '# To vote\nNo current entries\n\n## easy\nNo current entries\n\n## medium\nNo current entries\n\n## hard\nNo current entries\n' > PlayGame/VotePage/README.md
-echo -n "${{ inputs.targetURL }}" > PlayGame/currentStartImage.txt
+echo -n "${{ $TARGET_URL }}" > PlayGame/currentStartImage.txt
