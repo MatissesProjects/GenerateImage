52c4023a7f93405adf46be79a9e33b4127ea9512
Matisse <MatissesProjects@users.noreply.github.com>
2024-09-15 09:57:44 -0700

    attempt pull rebase false

--- workflowScripts/pull-then-push.sh
+++ workflowScripts/pull-then-push.sh
@@ -3,6 +3,9 @@
 # Ensure the required environment variables are set
 git config --global user.name "github-actions[bot]"
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
+# Set pull strategy to merge
+git config pull.rebase false
+
 git add .
 if [[ -n "$ISSUE_TITLE" && -n "$ISSUE_AUTHOR" ]]; then
   git commit -m "${ISSUE_TITLE} by ${ISSUE_AUTHOR}"
